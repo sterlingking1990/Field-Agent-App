87b3b3f96ab20c5d4f63d1499bda60538bd81883
Decagon <decagon@Darot.local>
2020-09-29 16:02:24 +0100

    setup files

---
+++ app/src/main/java/com/trapezoidlimited/groundforce/EntryApplication.kt
@@ -0,0 +1,7 @@
+package com.trapezoidlimited.groundforce
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EntryApplication:Application()
---
+++ app/src/main/java/com/trapezoidlimited/groundforce/ui/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.trapezoidlimited.groundforce.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trapezoidlimited.groundforce.R
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
